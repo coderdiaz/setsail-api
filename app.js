753e4fdbcf084eaf702ff7c7b1916a02260547ff
Javier Diaz <hello@coderdiaz.me>
2019-01-21 20:19:48 -0600

    feat: added error handling

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const createError = require('http-errors');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  // Returning API error in JSON
+  return res.status(statusCode).json({
+    code: statusCode,
+    message: err.message,
+  });
+});
 
 module.exports = app;
