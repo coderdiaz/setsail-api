50730b32cc1bd6bdc376014e0edc98051bcd7493
Javier Diaz <hello@coderdiaz.me>
2019-01-23 21:25:17 -0600

    feat: added login logic

--- routes/Auth.js
+++ routes/Auth.js
@@ -3,10 +3,49 @@ const router = express.Router();
 const { UserModel } = require('../models/UserModel');
 const Validator = require('validatorjs');
 const firebase = require('firebase');
+const jwt = require('jsonwebtoken');
 
 // POST /login
 router.post('/login', (req, res, next) => {
-  return res.json({});
+  const { body } = req;
+  const rules = {
+    email: 'required|email',
+    password: 'required',
+  };
+  const validation = new Validator(body, rules);
+  if (validation.fails()) {
+    const { errors: { errors } } = validation;
+    return res.status(400).json({
+      code: 400,
+      errors,
+    });
+  }
+  const { email, password } = body;
+  firebase.auth().signInWithEmailAndPassword(email, password).then(async auth => {
+    const user = await UserModel.findOne({email}).exec();
+    if (!user) {
+      return res.status(401).json({
+        code: 401,
+        message: 'Unauthorized',
+      });
+    }
+    const token = jwt.sign({ user }, process.env.JWT_SECRETKEY, {
+      expiresIn: process.env.JWT_EXPIRESIN,
+    });
+    const refreshToken = jwt.sign({ user }, process.env.JWT_SECRETKEY, {
+      expiresIn: process.env.JWT_REFRESHTOKEN_EXPIRESIN,
+    });
+    return res.status(200).json({
+      code: 200,
+      token,
+      refreshToken,
+    });
+  }).catch(err => {
+    return res.status(401).json({
+      code: 401,
+      message: 'Unauthorized',
+    });
+  });
 });
 
 // POST /signup
